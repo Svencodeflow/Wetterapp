2226cdffe455ccb856340941e92c8ffabf250dd6
megbug <meghub@proton.me>
2023-03-30 15:12:17 +0200

    header done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,25 @@
 
 
 //////Data Api
-fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5f1a23c9ded07f2769af1ee3b31ec13c")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+const cityName = document.querySelector('#cityName');
+const iconline = document.querySelector('#icon-degree')
+
+const wetterApp = () => {
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5f1a23c9ded07f2769af1ee3b31ec13c")
+        .then((response) => response.json())
+        .then((data) => {
+                cityName.innerHTML = `Weather in ${data.name}, ${data.sys.country}`
+                console.log((data));
+                let temp = Number(data.main.temp - 273.15).toFixed(1);
+                let date = new Date()
+                iconline.innerHTML = `${temp}° Celsius <br>${date.toLocaleString()}`
+        });
+        
+}
+
+wetterApp()
+
+
 
 
 /*=================== 
