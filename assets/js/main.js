e2eaddddabb4a8e684cab31f90ffe64a37e64dc2
Svencodeflow <schlaakboot@gmail.com>
2023-03-30 13:52:20 +0200

    Zweite API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
 
-fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5f1a23c9ded07f2769af1ee3b31ec13c")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+//////Geo API
+// fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=5f1a23c9ded07f2769af1ee3b31ec13c")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+
+
+//////Data Api
+// fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5f1a23c9ded07f2769af1ee3b31ec13c")
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
 
 
 /*=================== 
