8ee2d7c4cb8fbd4a51dd05cfe3a4e031ac1fe1f5
Svencodeflow <schlaakboot@gmail.com>
2023-03-30 13:25:37 +0200

    API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+
+fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=5f1a23c9ded07f2769af1ee3b31ec13c")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+
 /*=================== 
         Megan 
 =====================*/
